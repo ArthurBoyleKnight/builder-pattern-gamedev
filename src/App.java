bb36ee6d2554d8033f88d3ef9ca10d747e1eff68
M1sterPr1nce <maxgmgcreate@gmail.com>
2026-09-10 19:13:38 +0500

    forgot to change makeKnight to makeWizard to show 2 ways :(

--- src/App.java
+++ src/App.java
@@ -1,13 +1,19 @@
 public class App {
     public static void main(String[] args) {
         CharacterDirector director = new CharacterDirector();
-        CharacterObjectBuilder objBuilder = new CharacterObjectBuilder();
-        director.makeKnight(objBuilder);
-        GameCharacter knight = objBuilder.getResult();
-        System.out.println(knight);
+
+        CharacterObjectBuilder objectBuilder = new CharacterObjectBuilder();
+        director.makeKnight(objectBuilder);
+        GameCharacter knight = objectBuilder.getResult();
+
         CharacterTextBuilder textBuilder = new CharacterTextBuilder();
-        director.makeKnight(textBuilder);
-        String textCard = textBuilder.getResult();
-        System.out.println(textCard);
+        director.makeWizard(textBuilder);
+        String wizardTextCard = textBuilder.getResult();
+
+        System.out.println("The Hero:");
+        System.out.println(knight);
+
+        System.out.println("\nThe main character:");
+        System.out.println(wizardTextCard);
     }
 }
